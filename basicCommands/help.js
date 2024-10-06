d126facaa867c4088cc0b8ea7eae7cf4e03ca309
aqild65 <159115802+aqild65@users.noreply.github.com>
2024-10-06 23:58:02 +0800

    Update help.js

--- basicCommands/help.js
+++ basicCommands/help.js
@@ -57,21 +57,21 @@ module.exports = {
         }
       )
       .setThumbnail(botUser.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
-      .setImage(`https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=6641e8b7&is=66409737&hm=96f69ba8bcde4ca44bf8713e22001e0bafad0335cfd1f57c439a34b4762f0e03&`);
+      .setImage(`https://media.discordapp.net/attachments/948782888407674901/1241116948847268041/1_BARU.gif?ex=670393fb&is=6702427b&hm=0c9c69d848b89acca5549641a1a341eb164cae48cc051328a13b04d14a55ef4c&=`);
 
     const button1 = new ButtonBuilder()
       .setLabel('YouTube')
-      .setURL('https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A')
+      .setURL('https://www.youtube.com/@aqldnl')
       .setStyle(ButtonStyle.Link);
 
     const button2 = new ButtonBuilder()
       .setLabel('Discord')
-      .setURL('https://discord.gg/xQF9f9yUEM')
+      .setURL('https://discord.gg/dkatana')
       .setStyle(ButtonStyle.Link);
 
     const button3 = new ButtonBuilder()
       .setLabel('Code')
-      .setURL('https://github.com/GlaceYT')
+      .setURL('https://github.com/aqild65')
       .setStyle(ButtonStyle.Link);
       
     const row = new ActionRowBuilder()
